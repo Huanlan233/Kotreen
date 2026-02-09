556d0f674d9538f6cb0832e28fd0bf221beed9db
Huanlan233 <huanlan233@outlook.com>
2026-02-09 23:06:35 +0800

    feat: 多版本控制

--- src/client/kotlin/top/htext/kotreen/KotreenClient.kt
+++
@@ -1,9 +0,0 @@
-package top.htext.kotreen
-
-import net.fabricmc.api.ClientModInitializer
-
-object KotreenClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// TODO 游戏内GUI实现
-	}
-}
